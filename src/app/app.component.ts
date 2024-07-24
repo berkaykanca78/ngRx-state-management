431d864ce28108c50339e0e8b3e11a2414a9ebfe
Berkay Kanca <berkaykanca@hotmail.com>
2024-07-25 01:56:02 +0300

    Counter Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { MyCounterComponent } from "./my-counter/my-counter.component";
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MyCounterComponent, AsyncPipe],
+  template: `
+  <app-my-counter></app-my-counter>
+  `
 })
 export class AppComponent {
-  title = 'ngRx-state-management';
+  count$: Observable<number>;
+  count: number = 0;
+
+  constructor(
+    private store: Store<{ count: number }>
+  ) {
+    this.count$ = this.store.select("count");
+    this.store.select("count").subscribe(res => {
+      this.count = res;
+    })
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore({ count: counterReducer }),
+  ]
 };
